cb1e2206b0550561249c7134feef9da262f17bf7
Luis <listana@unsa.edu.pe>
2022-08-19 19:04:26 -0500

    Actualización del pipeline

--- src/main/java/weather/Weather.java
+++ src/main/java/weather/Weather.java
@@ -1,4 +1,4 @@
-package myapps;
+package weather;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
@@ -17,25 +17,32 @@ import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class Weather {
-
+    
     public static void main(String[] args) throws Exception {
+        // create properties
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "Weather-collector");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        // create topology
         final StreamsBuilder builder = new StreamsBuilder();
-
+        // create stream from input topic
         KStream<String, String> source = builder.stream("weather-1");
-        source.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
-              .groupBy((key, value) -> value)
-              .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
-              .toStream()
-              .to("weather-2", Produced.with(Serdes.String(), Serdes.Long()));
-
-
+        // filter out empty lines
+        KStream<String, String> filtered = source.filter((key, value) -> !value.isEmpty());
+        // split lines into words
+        KStream<String, String> words = filtered.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split(";")));
+        // store word and temperature
+        words.map((key, value) -> new KeyValue<>(value, key))
+                .groupByKey()
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
+        
         final Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+
         final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
